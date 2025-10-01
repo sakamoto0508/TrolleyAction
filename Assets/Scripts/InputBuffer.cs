5ae2ac49c6f39802c8b21958d8fe23d75f09624a
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-10-01 15:00:44 +0900

    [add]inputbuffer

--- Assets/Scripts/InputBuffer.cs
+++ Assets/Scripts/InputBuffer.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class InputBuffer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private const string MOVE_ACTION = "Move";
+    private const string JUMP_ACTION = "Jump";
 
-    // Update is called once per frame
-    void Update()
+    public InputAction MoveAction => _moveAction;
+    public InputAction JumpAction => _jumpAction;
+
+    private InputAction _moveAction;
+    private InputAction _jumpAction;
+
+    private void Awake()
     {
-        
+        if(TryGetComponent<PlayerInput>(out var playerInput))
+        {
+            _moveAction = playerInput.actions[MOVE_ACTION];
+            _jumpAction = playerInput.actions[JUMP_ACTION];
+        }
     }
 }
